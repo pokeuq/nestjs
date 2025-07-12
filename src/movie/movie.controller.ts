75881873617c72abb84dcb3341f15bc0a90c91f2
Bezhan Khayriddinov <pokeuq@MacBook-Air-Bezhan.local>
2025-07-12 22:03:03 +0500

    feat: params decorators

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,19 +1,62 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Headers,
+  Req,
+  Res,
+  Param,
+} from '@nestjs/common';
+import type { Request, Response } from 'express';
 
-@Controller({ path: 'movies', host: ['api.teacoder.ru', 'api.google.com'] })
+@Controller('movies')
 export class MovieController {
   @Get()
-  findAll() {
-    return [
-      {
-        title: 'Fight Club',
-      },
-      {
-        title: 'The Matrix',
-      },
-      {
-        title: 'The Dark Knight',
-      },
-    ];
+  findAll(@Query() query: any) {
+    return JSON.stringify(query);
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string) {
+    return { id };
+  }
+  @Get(':id/:reviewId')
+  findByManyId1(@Param() params: any) {
+    return JSON.stringify(params);
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return JSON.stringify(body);
+  }
+
+  @Get('headers')
+  getHeaders(@Headers() headers: any) {
+    return JSON.stringify(headers);
+  }
+
+  @Get('headers/user-agent')
+  getUserAgentHeaders(@Headers('user-agent') userAgent: string) {
+    return { userAgent };
+  }
+
+  @Get('request')
+  getRequestDetails(@Req() req: Request) {
+    return {
+      method: req.method,
+      url: req.url,
+      headers: req.headers,
+      query: req.query,
+      params: req.params,
+    };
+  }
+
+  @Get('response')
+  getResponseDetails(@Res() res: Response) {
+    res.status(200).json({
+      message: 'Hello World',
+    });
   }
 }
