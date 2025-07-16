c8f4e7bb800b27d7afebd048d4c8768a0896d65d
Bezhan Khayriddinov <pokeuq@MacBook-Air-Bezhan.local>
2025-07-16 10:16:26 +0500

    feat: typeorm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getTypeOrmConfig } from './config/typeorm.config';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [MovieModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getTypeOrmConfig,
+      inject: [ConfigService],
+    }),
+    MovieModule,
+  ],
 })
 export class AppModule {}
